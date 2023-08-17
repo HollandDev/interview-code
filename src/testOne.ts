8d2874d5ec1b5e928d1a1191fcb35e2f7389764a
Dylan Holland <dylaneholland@gmail.com>
2023-08-17 12:16:28 -0700

    adding simple code

---
+++ src/testOne.ts
@@ -0,0 +1,17 @@
+export const displayStringList = (stringList) => {
+  /*
+        Write a function to take a list of strings
+        e.g. ['one', 'two', 'three']
+        and display it line per line with -> next to it
+        e.g.
+        -> one
+        -> two
+        -> three
+
+        When a list is nested it should have an additional dash representing the level
+        e.g. ['one', 'two', ['three']] becomes
+        -> one
+        -> two
+        --> three
+    */
+};
---
+++ src/testTwo.ts
@@ -0,0 +1,13 @@
+/*
+    You are given a list of two nodes that reflect the structure
+*/
+interface TreeNode {
+  parent: TreeNode | null;
+}
+
+/*
+    Implement a function that finds the common ancestor of both nodes or returns false
+    if they do not share an ancestor.
+*/
+
+export const getCommonAncestor = (nodeOne: TreeNode, nodeTwo: TreeNode) => {};
