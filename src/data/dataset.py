72e4895af1d2c639a06bf8b703d272c6531c4b5f
DylanEHolland <salinson1138@gmail.com>
2023-08-29 10:54:53 -0700

    Adding python code

---
+++ src/data/dataset.py
@@ -0,0 +1,54 @@
+data_set_one = ["one", "two", ["three", "four"], "five"]
+data_set_two = ["one", "", "two", "", "", ["three", "", "four"]]
+data_set_three = [
+  "one",
+  1,
+  "",
+  "",
+  "two",
+  2,
+  3,
+  4,
+  5,
+  [
+    "",
+    "three",
+    [
+      "four",
+      "",
+      "",
+      "     ",
+      [
+        "five",
+        "",
+        "              ",
+        " six ",
+        ["seven              ", "eight", ["nine"]],
+      ],
+    ],
+  ],
+  "ten",
+  "       ",
+  "       ",
+  0.18,
+  0x100,
+  lambda x : x + 1,
+  [
+    [
+      [
+        [
+          [
+            [
+              [
+                [
+                  "       eleven      ",
+                  [[[[["twelve", [[[[[[["thirteen"]]]]]]]]]]]],
+                ],
+              ],
+            ],
+          ],
+        ],
+      ],
+    ],
+  ],
+];
---
+++ src/test_one.py
@@ -0,0 +1,21 @@
+
+from data.dataset import data_set_one, data_set_two, data_set_three
+def display_string_list(str_list):
+    """
+        Write a function to take a list of strings
+        e.g. ['one', 'two', 'three']
+        and display it line per line with -> next to it
+        e.g.
+        -> one
+        -> two
+        -> three
+
+        When a list is nested it should have an additional dash representing the level
+        e.g. ['one', 'two', ['three']] becomes
+        -> one
+        -> two
+        --> three
+    """
+    pass
+
+print(data_set_one)
